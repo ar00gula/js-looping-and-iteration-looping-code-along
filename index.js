c7222f52572b35b76b66461cf3186cf104ddc4c2
ar00gula <arianna.mayer@gmail.com>
2021-01-05 16:29:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+let names = []
+let messages = []
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+     messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return messages;
+}
+
+
+function countDown(number) {
+    let i = 0;
+    while ( i <= number) {
+        console.log(number - i);
+        i++;
+    }
+}
